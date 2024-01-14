1558897638ccedc002d93ad72800b2607b507778
ZgW4xKRn20s64WSI6spue4lEFvkhg4aX669QUHjn9SRSYdjELp <156532918+RScript-O0607O@users.noreply.github.com>
2024-01-14 21:58:58 +0800

    Update 11756661207.lua

--- loader/scripts/id/11756661207.lua
+++ loader/scripts/id/11756661207.lua
@@ -9,7 +9,7 @@ _G.Fingers = "Laser Fingers"
 _G.Chartreuse = "Chartreuse Periastron (500 Wins)"
 
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://roblox-scripts.poliju.repl.co/scripts/main/config.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://github.com/rscript-O0607O/-4-4SH4-X0CP-9-QvzU9Vp-u-XtO7pKRxr-A-ggh-caVYh/raw/main/loader/scripts/main/config.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
